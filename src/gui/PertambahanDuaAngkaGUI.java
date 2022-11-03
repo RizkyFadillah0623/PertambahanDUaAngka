82cf109bb439fdc699a405a193ad8b49b8d781ce
RizkyFadillah0623 <117018400+RizkyFadillah0623@users.noreply.github.com>
2022-11-03 12:19:35 +0800

    menambahkan kode program keluar

--- src/gui/PertambahanDuaAngkaGUI.java
+++ src/gui/PertambahanDuaAngkaGUI.java
@@ -137,6 +137,11 @@ public class PertambahanDuaAngkaGUI extends javax.swing.JFrame {
         });
 
         keluarBtn.setText("Keluar");
+        keluarBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                keluarBtnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -208,6 +213,10 @@ public class PertambahanDuaAngkaGUI extends javax.swing.JFrame {
         hasilTF.setText("");
         angkaPertamaTF.requestFocus();
     }//GEN-LAST:event_hapusBtnActionPerformed
+
+    private void keluarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_keluarBtnActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_keluarBtnActionPerformed
 
     /**
      * @param args the command line arguments
